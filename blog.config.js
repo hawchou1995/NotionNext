b406fe3d75b3fc9022caa3e8bc5daa14de66ce0c
hawchou1995 <45247266+hawchou1995@users.noreply.github.com>
2023-01-29 19:18:12 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -11,8 +11,8 @@ const BLOG = {
   APPEARANCE: 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
 
   AUTHOR: '情报总橘', // 作者
-  BIO: '一枚没有感情的小橘子🍚', // 作者简介
-  LINK: 'https://xauthub.top', // 网站地址
+  BIO: '一枚没有感情的小橘子🍊', // 作者简介
+  LINK: 'https://hexo.xauthub.top', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: 'hawchow@qq.com', // 邮箱
