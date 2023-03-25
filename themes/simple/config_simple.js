e3d94cc6618bdb04e641f46d94c31e2115b01281
hawchou1995 <45247266+hawchou1995@users.noreply.github.com>
2023-03-25 13:21:21 +0800

    Update config_simple.js

--- themes/simple/config_simple.js
+++ themes/simple/config_simple.js
@@ -3,7 +3,7 @@ const CONFIG_SIMPLE = {
   LOGO_IMG: '/Logo.webp',
   TOP_BAR: true, // 显示顶栏
   TOP_BAR_CONTENT: process.env.NEXT_PUBLIC_THEME_SIMPLE_TOP_TIPS || '',
-  LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>不做千篇一律的墙<br/要做独一无二的橘/div>',
+  LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>不做千篇一律的墙<br/>要做独一无二的橘</div>',
 
   AUTHOR_LINK: process.env.NEXT_PUBLIC_AUTHOR_LINK || '#',
 
